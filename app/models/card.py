dd79d2116e4624251811a2cc89005fc1f76ef2aa
mckay <mckaybwarren@gmail.com>
2023-02-04 17:48:37 -0500

    restrict card message attribute to 40 chars

--- app/models/card.py
+++ app/models/card.py
@@ -2,5 +2,5 @@ from app import db
 
 class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    message = db.Column(db.String, nullable=False)
+    message = db.Column(db.String(40), nullable=False)
     likes_count = db.Column(db.Integer, default=0)
