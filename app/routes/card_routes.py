3ff2b56de5ac5d8684861f94baaebcc6515721f8
nadxelle <nadxellehernandez@gmail.com>
2023-02-06 13:10:59 -0800

    added update likes of card

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -16,8 +16,20 @@ def delete_card(card_id):
         "statuscode": 200,
         "message": f"Card with id: {card_id} successfully deleted"
     }
+    return make_response(jsonify(response_obj),200)
 
-# cards_bp.route("/<card_id>/add-likes", methods=["PATCH"])
-# def add_likes_to_card(card_id):
-#     card = validate_model(Card,card_id)
+cards_bp.route("/<card_id>/add-likes", methods=["PATCH"])
+def add_likes_to_card(card_id):
+    card = validate_model(Card,card_id)
+
+    card.likes += 1
+
+    db.session.commit()
+
+    response_obj = {
+        "statuscode": 200,
+        "message": f"Increased likes for card id {card_id} to {card.likes}",
+        "data": card.to_dict()
+    }
+    return make_response(jsonify(response_obj),200)
 
