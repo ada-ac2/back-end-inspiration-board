4cadc4ad1344ab90663c7fb44da0cc378a86f6ee
mckay <mckaybwarren@gmail.com>
2023-02-05 20:55:50 -0500

    generate migration for one to many relationship between board and card models

--- app/models/board.py
+++ app/models/board.py
@@ -6,4 +6,4 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
-    cards = db.relationship("Moon", back_populates="planet")
+    cards = db.relationship("Card", back_populates="board")
