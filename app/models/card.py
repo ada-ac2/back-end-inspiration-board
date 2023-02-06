b96b1d0048c55dd5e0cef3dfeb33dfceb620db92
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-02-06 12:46:07 -0800

    created migration files for Card and Board models, upgraded db, added seperate folder for routes and seperate files for tests for models and routes

--- app/models/card.py
+++ app/models/card.py
@@ -7,6 +7,6 @@ class Card(db.Model):
     likes_count = db.Column(db.Integer, default=0)
     display_status = db.Column(db.Boolean, default=True)
     # make sure of the name of the "backpopulates" in the board model
-    board_id = db.Column(db.Intger, db.ForeignKey(board.id))
+    board_id = db.Column(db.Integer, db.ForeignKey("board.id"))
     board = db.relationship("Board", back_populates="cards")
     
