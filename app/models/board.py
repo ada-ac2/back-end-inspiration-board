05d3d4ccb111ef543e9a5475ae3180efeb286336
nadxelle <nadxellehernandez@gmail.com>
2023-02-05 16:55:41 -0800

    Created basic setup, models, migrations, folder structure

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    title = db.Column(db.String(120), nullable=False)
+    owner = db.Column(db.String(120), nullable=False)
+    cards = db.relationship("Card", back_populates="board")
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    message = db.Column(db.String, nullable=False)
+    likes = db.Column(db.Integer,nullable=False, default=0)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+    board = db.relationship("Board", back_populates="cards")
