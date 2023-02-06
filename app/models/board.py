1837fc0b14df7b36f7b67b9e0c977947ecc05e69
mckay <mckaybwarren@gmail.com>
2023-02-05 20:59:40 -0500

    fix typo on board model

--- app/models/board.py
+++ app/models/board.py
@@ -5,6 +5,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
     owner = db.Column(db.String, nullable=False)
+    cards = db.relationship("Card", back_populates="board")
 
     def to_dict(self):
         return {
