fee89f1342f4eda2aa92858c035b1792570c109f
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-02-06 13:34:46 -0800

    added from_dict and to_dict methods in Card model file, added add_two_boards fixture, added two tests for from_dict method in test_card_model file

---
+++ tests/test_card_model.py
@@ -0,0 +1,21 @@
+from app.models.card import Card
+import pytest
+
+
+def test_from_dict_with_valid_input(client, add_two_boards):
+    test_data = {
+        "message" : "Enjoy coding everyone!"
+    }
+
+    result = Card.from_dict(test_data, 1)
+
+    assert result.message == "Enjoy coding everyone!"
+    assert result.board_id == 1
+
+def test_from_dict_without_message_input_raises_error(client, add_two_boards):
+    test_data = {
+        "messageS" : "Enjoy coding everyone!"
+    }
+
+    with pytest.raises(KeyError, match="message"):
+        Card.from_dict(test_data, 1)
