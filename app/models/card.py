fbda9847d17521e8b561a624a7a3b1ed4de0e0eb
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-02-06 13:44:49 -0800

    corrected status column name for the Card model

--- app/models/card.py
+++ app/models/card.py
@@ -5,7 +5,7 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     message = db.Column(db.String, nullable=False)
     likes_count = db.Column(db.Integer, default=0)
-    display_status = db.Column(db.Boolean, default=True)
+    status = db.Column(db.Boolean, default=True)
     # make sure of the name of the "backpopulates" in the board model
     board_id = db.Column(db.Integer, db.ForeignKey("board.id"))
     board = db.relationship("Board", back_populates="cards")
