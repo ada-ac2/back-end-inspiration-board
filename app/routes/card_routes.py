44d463b32a3c059e3449085f73a36a88f3ce6363
nadxelle <nadxellehernandez@gmail.com>
2023-02-06 12:58:19 -0800

    Delete card endpoint

---
+++ app/routes/card_routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, jsonify, make_response
+from app.routes.helpers import validate_model
+from app.models.card import Card
+from app import db
+
+cards_bp = Blueprint("cards_bp", __name__, url_prefix="cards")
+
+cards_bp.route("/<card_id>", methods=["DELETE"])
+def delete_card(card_id):
+    card = validate_model(Card, card_id)
+
+    db.session.delete(card_id)
+    db.session.commit()
+
+    response_obj = {
+        "statuscode": 200,
+        "message": f"Card with id: {card_id} successfully deleted"
+    }
+
+
