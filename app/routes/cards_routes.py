f8e0bd4c9553256c0c18959fb814fa6b4eb25883
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-02-06 21:02:20 -0800

    added like_card and get_all_card (for one board) routes

--- app/routes/cards_routes.py
+++ app/routes/cards_routes.py
@@ -1,5 +1,5 @@
 from app import db
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, jsonify
 from app.models.card import Card
 from app.models.board import Board
 
@@ -35,7 +35,7 @@ def create_card(board_id):
     return make_response({"message":f"Card #{new_card.id} successfully added to {board.title} board"}, 201)
 
 
-@cards_bp.route("/<card_id>", methods=["PATCH"])
+@cards_bp.route("/archive/<card_id>", methods=["PATCH"])
 def delete_card(board_id, card_id):
     board = validate_model(Board, board_id)
     card = validate_model(Card, card_id)
@@ -44,3 +44,25 @@ def delete_card(board_id, card_id):
     return make_response(
         {"message":f"card #{card_id} has been successfully removed from the {board.title} board"}, 200
     )
+
+
+@cards_bp.route("/like/<card_id>", methods=["PATCH"])
+def like_card(board_id, card_id):
+    board = validate_model(Board, board_id)
+    card = validate_model(Card, card_id)
+    card.likes_count += 1
+    db.session.commit()
+    return make_response(
+        {"message":f"card #{card_id} has been liked. It has {card.likes_count} likes now"}, 200
+    )
+
+
+@cards_bp.route("", methods=["GET"])
+def get_all_cards(board_id):
+    board = validate_model(Board, board_id)
+    all_cards_in_board = Card.query.filter(Card.status == True).all()
+    cards_response = []
+    for card in all_cards_in_board:
+        cards_response.append(card.to_dict())
+
+    return jsonify(cards_response), 200
