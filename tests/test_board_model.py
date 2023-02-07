79e9d35489f9aaae0418f8ae4bc494881afea07d
yves <dwu415@gmail.com>
2023-02-07 13:46:58 -0800

    test board model

--- tests/test_board_model.py
+++ tests/test_board_model.py
@@ -10,8 +10,30 @@ def test_from_dict_with_valid_input():
     }
     result = Board.from_dict(test_data)
     # Assert
-    assert result.id == 1
-    assert result.status == True
-    assert result.selected == False
     assert result.title == "Test Title 12"
     assert result.owner == "Test Owner 12"
+
+def test_from_dict_with_missing_attribute():
+    # Act
+    test_data = {
+        "": "Test Title 12",
+        "owner": "Test Owner 12"
+    }
+    # Assert
+    with pytest.raises(KeyError, match="title"):
+        Board.from_dict(test_data)
+
+def test_to_dict_with_data():
+    # Act
+    test_data = {
+        "title": "Test Title 12",
+        "owner": "Test Owner 12"
+    }
+    result = Board.from_dict(test_data).to_dict()
+    # Assert
+    assert len(result) == 5
+    assert result["id"] == None
+    assert result["title"] == "Test Title 12"
+    assert result["owner"] == "Test Owner 12"
+    assert result["status"] == None
+    assert result["selected"] == None
