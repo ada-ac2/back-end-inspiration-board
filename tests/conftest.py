3258223cb610ce5c9bf08c917f56cdab7f8bedef
Jennifer Dai <jendai173@gmail.com>
2023-02-06 17:51:33 -0800

    add test fixture and 5 tests for boards route

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app import create_app
 from app import db
+from app.models.board import Board
 
 
 @pytest.fixture
@@ -20,3 +21,21 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def saved_two_boards(app):
+    board1 = Board(title="Hello,world!",
+                owner="Nad",
+                cards = []
+                )
+
+    board2 = Board(title="Hello,friend!",
+                owner="Jennifer",
+                cards = []
+                )
+
+    db.session.add_all([board1, board2])
+    db.session.commit()
+    db.session.refresh(board1, ["id"])
+    db.session.refresh(board2, ["id"])
