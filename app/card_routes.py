3960550090aafa8f9ce0217b5cbf873ed4b9d679
Sheung Wan Wong <melodywonghxy@hotmail.com>
2023-02-06 20:13:44 -0700

    add routes_helper python file

--- app/card_routes.py
+++ app/card_routes.py
@@ -5,3 +5,12 @@ from app.models.card import Card
 
 
 cards_bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
+
+@cards_bp.route("/<id>", methods=["DELETE"])
+def delete_board(id):
+    board = validate_model(Board, id)
+
+    db.session.delete(board)
+    db.session.commit()
+
+    return make_response(f"Board #{id} successfully deleted")
