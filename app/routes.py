9324c29415df1252052b3d87f075458677d0b18a
maple-megan333 <maple.megan333@gmail.com>
2023-02-06 17:57:23 -0800

    updated post

--- app/routes.py
+++ app/routes.py
@@ -13,9 +13,10 @@ board_bp = Blueprint("board_bp", __name__, url_prefix="/boards")
 @board_bp.route("", methods = ["POST"])
 def create_board():
     request_body = request.get_json()
-    new_board = Board(
-        title = request_body["title"],
-        owner = request_body["owner"])
+    try:
+        new_board = Board.from_dict(request_body)
+    except KeyError as key_error:
+        abort(make_response({"message": f"Bad request: {key_error.args[0]} attribute is missing"}, 400))
     db.session.add(new_board)
     db.session.commit()
     
@@ -23,7 +24,7 @@ def create_board():
         jsonify({
                 "title" : new_board.title,
                 "owner" : new_board.owner 
-            }), 200)
+            }), 201)
 
 # Monica
 # Add a card to a board by board id 
